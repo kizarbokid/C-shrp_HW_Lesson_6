4a3ddae9b09f9d627db1b826d41c312fd0322445
YakovkinVD <dikobrazzzik709@gmail.com>
2022-10-21 22:04:30 +0900

    Task_41 solved

---
+++ Task_41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+System.Console.Clear();
+
+System.Console.WriteLine("Введите число и нажмите Enter или введите 'Q' чтобы выйти: ");
+string n = "0";
+int count = 0;
+
+while (n.ToLower() != "q") 
+{
+    n = System.Console.ReadLine();
+
+    if (int.TryParse(n, out int result)) //ловим, число ли ввел пользователь?
+    {
+        if (int.Parse(n) > 0) //Да-->прирастить счетчик
+        {
+            count++;
+        }
+    }
+}
+
+
+System.Console.Write($"Количество введенных чисел больше нуля = {count}");
